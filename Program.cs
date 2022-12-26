76e383f3f76aaa3c57a1c46baad1e71779a7ae4b
arXaik <araik200414@gmail.com>
2022-12-26 02:06:22 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassLab
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var worker = new Worker("Иванов", 20000, "1972-12-26");
+
+            Console.WriteLine(worker.GetAge());
+            Console.WriteLine(worker.GetDaysTo50Year());
+        }
+    }
+}
